a7bfbf6e5bda69b7192540ca603b1668f0fac56e
Yonatan Kawo <yonas478@gmail.com>
2021-12-08 09:44:21 -0600

    commit tree 90e6d538bb56fbba1c1ba0f069529e6e29fe677f

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
 
 
+class Book 
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title,author,page_count,genre)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+end
+
+book1 = Book.new("Harry Potter", "Yonatan Kawo", 100, "Fiction")
